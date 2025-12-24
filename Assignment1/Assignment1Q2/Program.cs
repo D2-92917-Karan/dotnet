3387625a9ceec8b2366ec81e5863a9e73381f8d9
Karan5663 <132004682+Karan5663@users.noreply.github.com>
2025-12-24 15:13:19 +0530

    Assignment1 Completed

---
+++ Assignment1/Assignment1Q2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static int program(int x, int b, int c)
+        { int res=0;
+            switch (x)
+            {
+                case 1:
+                    res = b + c;
+                    break;
+                case 2:
+                    res = b - c;
+                    break;
+                case 3:
+                    res = b * c;
+                    break;
+                case 4:
+                    res = b / c;
+                    break;
+                case 5:
+                    break;
+            }
+            return res;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Enter Numnber1:");
+            string str1= Console.ReadLine();
+            int num1=int.Parse(str1);
+            Console.WriteLine("Enter Number 2");
+            string str2 = Console.ReadLine();
+            int num2 = int.Parse(str2);
+            Console.WriteLine("Enter the cohice");
+            Console.WriteLine("1.Addoition");
+            Console.WriteLine("2.Sub");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Division");
+            string op1 = Console.ReadLine();
+            int op = int.Parse(op1);
+            Console.WriteLine("Result :"+program(op,num1,num2));
+        }
+    }
+}
---
+++ Assignment1/Assignment1Q3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Q3
+{
+    class Person
+    {
+        private string name;
+        private Boolean gender;
+        private int age;
+        private int std;
+        private Char div;
+        private Double marks;
+
+        public Person() : this(" ", false, 0, 0, '\\', 0.0)
+        {
+
+        }
+        public Person(string name, bool gender, int age, int std, char div, double marks)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.std = std;
+            this.div = div;
+            this.marks = marks;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public Boolean Gender
+        {
+            get { return this.gender; }
+            set { this.gender = value; }
+        }
+        public int Age
+        {
+            get{ return this.age; }
+            set { this.age = value; }
+        }
+        public int Std
+        {
+            get{ return this.std; } 
+            set { this.std = value; }
+
+        }
+        public Char Div
+        {
+            get{ return this.div; }
+            set { this.div = value; }
+        }
+        public Double Marks
+        {
+            get { return this.marks; }
+            set{ this.marks = value; }
+        }
+
+        public void printRecord()
+        {
+            Console.WriteLine("User Details:");
+            Console.WriteLine("Name :" + this.name);
+            Console.WriteLine("Gender :" +(this.gender ? "Male" :"FeMale"));
+            Console.WriteLine("age :" + this.age);
+            Console.WriteLine("std :" + this.std);
+            Console.WriteLine("div :" + this.div);
+            Console.WriteLine("marks :" + this.marks);
+        }
+                  
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the Name");
+            string name=Console.ReadLine();
+            Console.WriteLine("Enter True if Male otherwise false");
+           bool gender=bool.Parse(Console.ReadLine());
+            Console.WriteLine("Age : ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("std : ");
+            int std = int.Parse(Console.ReadLine());
+            Console.WriteLine("div : ");
+            Char div = char.Parse(Console.ReadLine());
+            Console.WriteLine("Marks : ");
+            double marks = double.Parse(Console.ReadLine());
+            Person person = new Person(name,gender,age,std,div,marks);
+            person.printRecord();
+
+        }
+    }
+}
